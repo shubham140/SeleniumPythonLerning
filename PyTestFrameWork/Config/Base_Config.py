21cedc7ccc3962adf7ee9e0e86ddbefee75f5ff8
SHUBHAM CHAWLA <shubhamchawla140@gmail.com>
2021-05-30 14:01:49 +0530

    Add files via upload

---
+++ PyTestFrameWork/Config/Base_Config.py
@@ -0,0 +1,7 @@
+
+"""Here we will add all static paths and data which will remain constant i.e.., Like Chrome driver,Gecko Driver executable Path if required if you are not using
+WebDriverManager and may more"""
+
+# If you see pass written means all the data is cycling from different func and classes
+class Config_Base:
+    URL="https://opensource-demo.orangehrmlive.com/index.php/dashboard"
